c5eb07133b7edd6ea87aad3760a6ef2897fecdcc
Paul Rossotti <rossotti.paul@gmail.com>
2013-10-03 22:06:41 -0700

    Change controlled to pass default date to list()

--- basketball/app/controllers/Games.java
+++ basketball/app/controllers/Games.java
@@ -13,11 +13,12 @@ public class Games extends Controller {
     * @param page Current page number (starts from 0)
     */
    public static Result list(int page) {
+//	   java.text.SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", java.util.Locale.ENGLISH);
+//	   String gameDate = simpleDateFormat.format(new java.util.Date()).toString();
+	   String gameDate = "2012-10-31";
+
        return ok(
-           listGames.render(
-               Game.page(page, 15)
-           )
+     		  listGames.render(Game.pageByDate(page, 15, gameDate))
        );
    }
 }
-
