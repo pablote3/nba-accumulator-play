35a6d1f71489438f339fa73b4a200be6787b74ab
Paul Rossotti <rossotti.paul@gmail.com>
2013-10-09 21:28:12 -0700

    Update default todayDate()

--- basketball/public/javascripts/aggregator.js
+++ basketball/public/javascripts/aggregator.js
@@ -1,6 +1,6 @@
 function todayDate() {
 	var date = new Date();
 	<!-- var dateString = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear(); -->
-	var dateString = "10/29/2012";
+	var dateString = "10/31/2012";
 	return dateString;
 }
