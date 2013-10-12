f6365d4f68d2970f18c2d1bcb0c3e46c9b260643
Paul Rossotti <rossotti.paul@gmail.com>
2013-10-12 16:34:42 -0700

    Expand test cases

--- basketball/test/utilities/DateTimeTest.java
+++ basketball/test/utilities/DateTimeTest.java
@@ -12,23 +12,29 @@ import org.junit.Test;
 
 import util.DateTime;
 
-public class DateTimeTest {
-
+public class DateTimeTest {	
     @Test
-    public void displayTime() {
-    	String stringDate = "2013-03-30 19:30:00";
-    	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss", Locale.ENGLISH);    	
-    	
+    public void displayTimeHalfHour() {
+    	Date date = parseDate("2013-03-30 19:30:00");
+    	System.out.println(DateTime.getDisplayTime(date));
+    	assertThat(DateTime.getDisplayTime(date)).isEqualTo("19:30 PM PDT");
+    }
+    
+    @Test
+    public void displayTimeFullHour() {
+    	Date date = parseDate("2013-03-30 19:00:00");
+    	System.out.println(DateTime.getDisplayTime(date));
+        assertThat(DateTime.getDisplayTime(date)).isEqualTo("19:00 PM PDT");
+    }
+    
+    public Date parseDate(String stringDate) {
+    	SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss", Locale.ENGLISH); 
     	Date date = null;
 		try {
 			date = simpleDateFormat.parse(stringDate);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-    	
-        String displayTime = DateTime.getDisplayTime(date);
-        System.out.println(displayTime);
-
-        assertThat(displayTime.equalsIgnoreCase("19:30 PM"));
+		return date;
     }
 }
