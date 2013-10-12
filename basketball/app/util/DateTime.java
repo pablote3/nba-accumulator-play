dc879163b7deaa3db6142c7a1cd4d20bed625661
Paul Rossotti <rossotti.paul@gmail.com>
2013-10-12 16:35:07 -0700

    Switch from Calendar to SimpleDateFormatter

--- basketball/app/util/DateTime.java
+++ basketball/app/util/DateTime.java
@@ -1,26 +1,12 @@
 package util;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class DateTime {
-	
 	static public String getDisplayTime(Date date) {
-		Calendar calendar = new GregorianCalendar();
-		calendar.setTime(date);
-		
-		return new StringBuffer()
-				.append(calendar.get(Calendar.HOUR_OF_DAY) + ":")
-				.append(calendar.get(Calendar.MINUTE) + " ")
-				.append(DateTime.getTwelveHourClock(calendar.get(Calendar.AM_PM)))
-				.toString();
-	}
-	
-	static public String getTwelveHourClock(int period) {
-		if (period == 0)
-			return "AM";
-		else
-			return "PM";
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("kk:mm a z", Locale.ENGLISH);
+		return simpleDateFormat.format(date);
 	}
 }
