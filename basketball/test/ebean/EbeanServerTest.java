f595528859e852ff1cb37b99bf6384fc6cab4f7b
Paul Rossotti <rossotti.paul@gmail.com>
2013-11-26 21:03:12 -0800

    Clean up

--- basketball/test/ebean/EbeanServerTest.java
+++ basketball/test/ebean/EbeanServerTest.java
@@ -1,22 +1,13 @@
 package ebean;
 
-import models.entity.BoxScore;
-import models.entity.Game;
-import models.entity.Team;
-
 import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
 import com.avaje.ebean.SqlRow;
 import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.config.ServerConfig;
 
-import controllers.Games;
-
 public class EbeanServerTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		ServerConfig config = new ServerConfig();
 		config.setName("test"); 
@@ -26,14 +17,10 @@ public class EbeanServerTest {
 		mySql.setUsername("root");  
 		mySql.setPassword("root");  
 		mySql.setUrl("jdbc:mysql://localhost:3306/basketball");  
-//		mySql.setHeartbeatSql("select count(*) from team");  		  
+		mySql.setHeartbeatSql("select count(*) from team");  		  
 		config.setDataSourceConfig(mySql);
 		config.setDefaultServer(false);  
 		config.setRegister(false);
-		
-//		config.addClass(Game.class);
-//		config.addClass(BoxScore.class);
-//		config.addClass(Team.class);
 		
 		EbeanServer server = EbeanServerFactory.create(config);
 		
@@ -41,9 +28,5 @@ public class EbeanServerTest {
 		SqlRow row = server.createSqlQuery(sql).findUnique();
 		Integer i = row.getInteger("count");
 		System.out.println("Got " + i + " count");
-		
-		//find(Games.findKeyByDateTeam("2012-10-31", "sacramento-kings"));
-
 	}
-
 }
