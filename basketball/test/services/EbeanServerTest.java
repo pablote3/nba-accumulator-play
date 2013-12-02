683cc028e10af2b07d00168944a2e857770c14b8
Paul Rossotti <rossotti.paul@gmail.com>
2013-12-02 05:11:02 -0800

    Introduce EbeanServerTest

--- basketball/test/services/EbeanServerTest.java
+++ basketball/test/services/EbeanServerTest.java
@@ -2,15 +2,22 @@ package services;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import models.entity.Team;
+
+import org.junit.Test;
+
 import com.avaje.ebean.EbeanServer;
 import com.avaje.ebean.EbeanServerFactory;
 import com.avaje.ebean.SqlRow;
 import com.avaje.ebean.config.DataSourceConfig;
 import com.avaje.ebean.config.ServerConfig;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
-public class CreateEbeanServerTest {
+public class EbeanServerTest {
 
-	public static void main(String[] args) {
+    @Test
+    public void createEbeanServer() {
 		ServerConfig config = new ServerConfig();
 		config.setName("test"); 
 		
@@ -22,12 +29,29 @@ public class CreateEbeanServerTest {
 		mySql.setHeartbeatSql("select count(*) from team");  		  
 		config.setDataSourceConfig(mySql);
 		config.setDefaultServer(false);  
-		config.setRegister(false);
-		
+		config.setRegister(false);		
 		EbeanServer server = EbeanServerFactory.create(config);
 		
 		String sql = "select count(*) as count from team";
 		SqlRow row = server.createSqlQuery(sql).findUnique();
         assertThat(row.getInteger("count")).isEqualTo(31);
-	}
+    }
+    
+    @Test
+    public void invokeEbeanServerUsingSqlQuery() {
+		Injector injector = Guice.createInjector(new InjectorModule());		
+		EbeanServerService service = injector.getInstance(EbeanServerServiceImpl.class);		
+		EbeanServer server = service.createEbeanServer();
+		
+		String sql = "select count(*) as count from team";
+		SqlRow row = server.createSqlQuery(sql).findUnique();
+        assertThat(row.getInteger("count")).isEqualTo(31);
+    }
+    
+    @Test
+    public void invokeEbeanServerUsingTeamFinder() {
+		Team team = Team.findByKey("key", "sacramento-kings");
+        assertThat(team.getAbbr()).isEqualTo("SAC");
+    }
+    
 }
