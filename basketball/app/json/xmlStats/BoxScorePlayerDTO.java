4fbc0225905b9b0e4b5dbd9f004ec69dd5dddb39
Paul Rossotti <rossotti.paul@gmail.com>
2014-04-12 10:11:43 -0700

    Initial commit

---
+++ basketball/app/json/xmlStats/BoxScorePlayerDTO.java
@@ -0,0 +1,173 @@
+package json.xmlStats;
+
+public class BoxScorePlayerDTO {
+	private String last_name;
+	private String first_name;
+	private String display_name;
+	private String position;
+	private Short minutes;
+	private Short points;
+	private Short assists;
+	private Short turnovers;
+	private Short steals;
+	private Short blocks;
+	private Short field_goals_attempted;
+	private Short field_goals_made;
+	private Short three_point_field_goals_attempted;
+	private Short three_point_field_goals_made;
+	private Short free_throws_attempted;
+	private Short free_throws_made;
+	private Short defensive_rebounds;
+	private Short offensive_rebounds;
+	private Short personal_fouls;
+	private String team_abbreviation;
+	private Boolean is_starter;
+	private Float field_goal_percentage;
+	private Float three_point_percentage;
+	private Float free_throw_percentage;
+    
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getDisplay_name() {
+		return display_name;
+	}
+	public void setDisplay_name(String display_name) {
+		this.display_name = display_name;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public Short getMinutes() {
+		return minutes;
+	}
+	public void setMinutes(Short minutes) {
+		this.minutes = minutes;
+	}
+	public Short getPoints() {
+		return points;
+	}
+	public void setPoints(Short points) {
+		this.points = points;
+	}
+	public Short getAssists() {
+		return assists;
+	}
+	public void setAssists(Short assists) {
+		this.assists = assists;
+	}
+	public Short getTurnovers() {
+		return turnovers;
+	}
+	public void setTurnovers(Short turnovers) {
+		this.turnovers = turnovers;
+	}
+	public Short getSteals() {
+		return steals;
+	}
+	public void setSteals(Short steals) {
+		this.steals = steals;
+	}
+	public Short getBlocks() {
+		return blocks;
+	}
+	public void setBlocks(Short blocks) {
+		this.blocks = blocks;
+	}
+	public Short getFieldGoalAttempts() {
+		return field_goals_attempted;
+	}
+	public void setFieldGoalAttempts(Short field_goals_attempted) {
+		this.field_goals_attempted = field_goals_attempted;
+	}
+	public Short getFieldGoalMade() {
+		return field_goals_made;
+	}
+	public void setFieldGoalMade(Short field_goals_made) {
+		this.field_goals_made = field_goals_made;
+	}
+	public Short getThreePointAttempts() {
+		return three_point_field_goals_attempted;
+	}
+	public void setThreePointAttempts(Short three_point_field_goals_attempted) {
+		this.three_point_field_goals_attempted = three_point_field_goals_attempted;
+	}
+	public Short getThreePointMade() {
+		return three_point_field_goals_made;
+	}
+	public void setThreePointMade(Short three_point_field_goals_made) {
+		this.three_point_field_goals_made = three_point_field_goals_made;
+	}
+	public Short getFreeThrowAttempts() {
+		return free_throws_attempted;
+	}
+	public void setFreeThrowAttempts(Short free_throws_attempted) {
+		this.free_throws_attempted = free_throws_attempted;
+	}
+	public Short getFreeThrowMade() {
+		return free_throws_made;
+	}
+	public void setFreeThrowMade(Short free_throws_made) {
+		this.free_throws_made = free_throws_made;
+	}
+	public Short getReboundsDefense() {
+		return defensive_rebounds;
+	}
+	public void setReboundsDefense(Short defensive_rebounds) {
+		this.defensive_rebounds = defensive_rebounds;
+	}
+	public Short getReboundsOffense() {
+		return offensive_rebounds;
+	}
+	public void setReboundsOffense(Short offensive_rebounds) {
+		this.offensive_rebounds = offensive_rebounds;
+	}
+	public Short getPersonalFouls() {
+		return personal_fouls;
+	}
+	public void setPersonalFouls(Short personal_fouls) {
+		this.personal_fouls = personal_fouls;
+	}
+	public String getTeam_abbreviation() {
+		return team_abbreviation;
+	}
+	public void setTeam_abbreviation(String team_abbreviation) {
+		this.team_abbreviation = team_abbreviation;
+	}
+	public Boolean getStarter() {
+		return is_starter;
+	}
+	public void setStarter(Boolean is_starter) {
+		this.is_starter = is_starter;
+	}
+	public Float getFieldGoalPercent() {
+		return field_goal_percentage;
+	}
+	public void setFieldGoalPercent(Float field_goal_percentage) {
+		this.field_goal_percentage = field_goal_percentage;
+	}
+	public Float getThreePointPercent() {
+		return three_point_percentage;
+	}
+	public void setThreePointPercent(Float three_point_percentage) {
+		this.three_point_percentage = three_point_percentage;
+	}
+	public Float getFreeThrowPercent() {
+		return free_throw_percentage;
+	}
+	public void setFreeThrowPercent(Float free_throw_percentage) {
+		this.free_throw_percentage = free_throw_percentage;
+	}
+}
