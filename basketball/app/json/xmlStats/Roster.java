aa82019db3f58e722b282498b372dc5bda484675
Paul Rossotti <rossotti.paul@gmail.com>
2014-04-30 20:01:30 -0700

    Initial commit

---
+++ basketball/app/json/xmlStats/Roster.java
@@ -0,0 +1,9 @@
+package json.xmlStats;
+
+import models.Player;
+import models.Team;
+
+public class Roster {
+	public Team team;
+	public Player[] players;
+}
---
+++ basketball/test/xmlStats/RosterJsonFile.java
@@ -0,0 +1,65 @@
+package xmlStats;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import json.xmlStats.JsonHelper;
+import json.xmlStats.Roster;
+import models.Game.ProcessingType;
+import models.Player;
+
+import org.junit.Test;
+
+import util.DateTime;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class RosterJsonFile {
+
+    @Test
+    public void createRoster() {
+        running(fakeApplication(), new Runnable() {
+          public void run() {
+        	  InputStream baseJson; 
+        	  try {
+        		  Path path =  Paths.get(System.getProperty("config.test")).resolve("sacramento-kings_2013_14_complete.json");
+        		  File file = path.toFile();
+	              baseJson = new FileInputStream(file);
+		        
+	              ObjectMapper mapper = new ObjectMapper();
+	              mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	            
+	              Roster xmlStats = mapper.readValue(baseJson, Roster.class);
+	              
+	              List<Player> players = JsonHelper.getPlayers(xmlStats.players);
+	              
+	              for (int i = 0; i < players.size(); i++) {
+					Player.create(players.get(i), ProcessingType.online);
+					Player createPlayer = Player.findByNameBirthDate(players.get(i).getLastName(), players.get(i).getFirstName(), DateTime.getFindDateShort(players.get(i).getBirthDate()), ProcessingType.online);
+					assertThat(createPlayer.getBirthPlace()).isEqualTo(players.get(i).getBirthPlace());
+	              }
+	              
+//	              for (int j = 0; j < players.size(); j++) {
+//	            	  Player.delete(players.get(j), ProcessingType.online);
+//	              }
+
+        	  } catch (FileNotFoundException e) {
+      	          e.printStackTrace();
+      	      } catch (IOException e) {
+      	          e.printStackTrace();
+      	      }
+          }
+        });
+    }
+}
