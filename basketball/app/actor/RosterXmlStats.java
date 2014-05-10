b0130c065c3549fa2a480fbc229f786648f1ac7d
Paul Rossotti <rossotti.paul@gmail.com>
2014-05-10 06:46:14 -0700

    Introduce incomplete roster capability

--- basketball/app/actor/RosterXmlStats.java
+++ basketball/app/actor/RosterXmlStats.java
@@ -2,29 +2,29 @@ package actor;
 
 import static actor.ActorApi.InitializeComplete;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.zip.GZIPInputStream;
 
 import json.xmlStats.JsonHelper;
-import json.xmlStats.NBABoxScore;
-import models.BoxScore;
+import json.xmlStats.Roster;
 import models.Game;
-import models.GameOfficial;
-import models.PeriodScore;
-import models.BoxScore.Result;
-import models.Game.ProcessingType;
-import models.Game.Status;
+import models.Game.Source;
+import models.Player;
 import util.DateTime;
-import actor.ActorApi.CompleteGame;
-import actor.ActorApi.ScheduleGame;
 import actor.ActorApi.ServiceProps;
+import actor.ActorApi.UpdateRoster;
 import actor.ActorApi.XmlStatsException;
 import akka.actor.ActorRef;
-import akka.actor.Props;
 import akka.actor.UntypedActor;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -38,7 +38,8 @@ public class RosterXmlStats extends UntypedActor {
 	private String accessToken;
 	private String userAgentName;
 	private String urlRoster;
-	private ProcessingType processingType;
+	private String fileRoster;
+	private Source source;
 	private ActorRef listener;
 	
 	public RosterXmlStats(ActorRef listener) {
@@ -50,87 +51,61 @@ public class RosterXmlStats extends UntypedActor {
 			accessToken = "Bearer " + ((ServiceProps) message).accessToken;
 			userAgentName = ((ServiceProps) message).userAgentName;
 			urlRoster = ((ServiceProps) message).urlRoster;
-			processingType = Game.ProcessingType.valueOf(((ServiceProps) message).processType);
+			fileRoster = ((ServiceProps) message).fileRoster;
+			source = Game.Source.valueOf(((ServiceProps) message).source);
 			getSender().tell(InitializeComplete, getSelf());
 		}
-//		else if(message instanceof ScheduleGame) {
-//			URL url;
-//			InputStream baseJson = null;
-//			Game game = ((ScheduleGame)message).game;
-//			BoxScore awayBoxScore = game.getBoxScores().get(0);
-//			BoxScore homeBoxScore = game.getBoxScores().get(1);
-//			
-//			String urlDate = DateTime.getFindDateNaked(game.getDate());
-//			String urlAwayTeam = awayBoxScore.getTeam().getKey();
-//			String urlHomeTeam = homeBoxScore.getTeam().getKey();			
-//			String urlEvent = urlDate + "-" + urlAwayTeam + "-at-" + urlHomeTeam + ".json";
-//						
-//			try {
-//				url = new URL(urlBoxScore + urlEvent);
-//				URLConnection connection = url.openConnection();
-//				connection.setRequestProperty(AUTHORIZATION, accessToken);
-//				connection.setRequestProperty(USER_AGENT, userAgentName);
-//				connection.setRequestProperty(ACCEPT_ENCODING, GZIP);
-//				baseJson = connection.getInputStream();
-//				String encoding = connection.getContentEncoding();
-//				if (GZIP.equals(encoding)) {
-//					baseJson = new GZIPInputStream(baseJson);
-//				}
-//	            if (baseJson != null) {
-//	            	ObjectMapper mapper = new ObjectMapper();
-//	    	        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-//	    	        NBABoxScore xmlStatsBoxScore = mapper.readValue(baseJson, NBABoxScore.class);
-//	    	        
-//	    	        game.setStatus(Status.completed);
-//	    	        
-//	    	        if (game.getGameOfficials().size() > 0) {
-//	    	        	for (int i = 0; i < game.getGameOfficials().size(); i++) {
-//							GameOfficial.delete(game.getGameOfficials().get(i), processingType);
-//						}
-//	    	        }
-//	    	        game.setGameOfficials(JsonHelper.getGameOfficials(xmlStatsBoxScore.officials, processingType));
-//	    	        
-//	    	        if (awayBoxScore.getPeriodScores().size() > 0) {
-//	    	        	for (int i = 0; i < awayBoxScore.getPeriodScores().size(); i++) {
-//							PeriodScore.delete(awayBoxScore.getPeriodScores().get(i), processingType);
-//						}
-//	    	        }	    	        
-//	    	        awayBoxScore.setPeriodScores(JsonHelper.getPeriodScores(xmlStatsBoxScore.away_period_scores));
-//	    	        JsonHelper.getBoxScoreStats(awayBoxScore, xmlStatsBoxScore.away_totals);
-//	    	        awayBoxScore.setBoxScorePlayers(JsonHelper.getBoxScorePlayers(xmlStatsBoxScore.away_stats, DateTime.getFindDateShort(xmlStatsBoxScore.event_information.getDate()), processingType));
-//	    	        //is roster complete?
-//	    	        
-//	    	        if (homeBoxScore.getPeriodScores().size() > 0) {
-//	    	        	for (int i = 0; i < homeBoxScore.getPeriodScores().size(); i++) {
-//							PeriodScore.delete(homeBoxScore.getPeriodScores().get(i), processingType);
-//						}
-//	    	        }	 
-//	    	        homeBoxScore.setPeriodScores(JsonHelper.getPeriodScores(xmlStatsBoxScore.home_period_scores));
-//	    	        JsonHelper.getBoxScoreStats(homeBoxScore, xmlStatsBoxScore.home_totals);
-//	    	        homeBoxScore.setBoxScorePlayers(JsonHelper.getBoxScorePlayers(xmlStatsBoxScore.home_stats, DateTime.getFindDateShort(xmlStatsBoxScore.event_information.getDate()), processingType));
-//	    	        
-//	    		  	if (xmlStatsBoxScore.away_totals.getPoints() > xmlStatsBoxScore.home_totals.getPoints()) {
-//	    		  		homeBoxScore.setResult(Result.loss);
-//	    		  		awayBoxScore.setResult(Result.win);
-//	    		  	}
-//	    		  	else {
-//	    		  		homeBoxScore.setResult(Result.win);
-//	    		  		awayBoxScore.setResult(Result.loss);
-//	    		  	}
-//	    		  	CompleteGame cg = new CompleteGame(game);
+		else if(message instanceof UpdateRoster) {
+			String gameDate = ((UpdateRoster) message).date;
+			String nakedDate = DateTime.getFindDateNaked(DateTime.createDateFromStringDate(gameDate));
+			String gameTeam = ((UpdateRoster) message).team;
+			InputStream baseJson = null;
+			
+			try {
+				if (source.equals(Source.file)) {
+					String fileTeam =  gameTeam + "-" + nakedDate + ".json";				
+					Path path =  Paths.get(fileRoster).resolve(fileTeam);
+					File file = path.toFile();
+					baseJson = new FileInputStream(file);
+				}
+				else {
+					URL url;
+					String urlTeam = urlRoster + gameTeam + ".json";
+					url = new URL(urlTeam);
+					URLConnection connection = url.openConnection();
+					connection.setRequestProperty(AUTHORIZATION, accessToken);
+					connection.setRequestProperty(USER_AGENT, userAgentName);
+					connection.setRequestProperty(ACCEPT_ENCODING, GZIP);
+					baseJson = connection.getInputStream();
+					String encoding = connection.getContentEncoding();
+					if (GZIP.equals(encoding)) {
+						baseJson = new GZIPInputStream(baseJson);
+					}
+				}
+				if (baseJson != null) {
+				  	ObjectMapper mapper = new ObjectMapper();
+				    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+				    Roster xmlStatsRoster = mapper.readValue(baseJson, Roster.class);
+				    
+				    List<Player> activeRoster = JsonHelper.getPlayers(xmlStatsRoster.players);
+	    
+
 //	    		  	getSender().tell(cg, getSelf());
-//	            }
-//			} 
-//			catch (MalformedURLException e) {
-//				listener.tell(new XmlStatsException("MalformedURLException"), getSelf());
-//			} 
-//			catch (IOException e) {
-//				listener.tell(new XmlStatsException("IOException"), getSelf());
-//			}
-//			catch (Exception e) {
-//				System.out.println("Exception");
-//			}
-//		}
+				}
+			} 
+			catch (FileNotFoundException e) {
+				listener.tell(new XmlStatsException("FileNotFoundException"), getSelf());
+			}
+			catch (MalformedURLException e) {
+				listener.tell(new XmlStatsException("MalformedURLException"), getSelf());
+			} 
+			catch (IOException e) {
+				listener.tell(new XmlStatsException("IOException"), getSelf());
+			}
+			catch (Exception e) {
+				System.out.println("Exception");
+			}
+		}
 		else {
 			unhandled(message);
 		}
