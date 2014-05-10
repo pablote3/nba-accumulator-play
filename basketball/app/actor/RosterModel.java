ae744fdbc7a95fff2f301258c3158a38987d1a16
Paul Rossotti <rossotti.paul@gmail.com>
2014-05-10 06:44:51 -0700

    Introduce incomplete roster capability

--- basketball/app/actor/RosterModel.java
+++ basketball/app/actor/RosterModel.java
@@ -1,21 +1,9 @@
 package actor;
 
-import static actor.ActorApi.NextGame;
-import static actor.ActorApi.WorkStart;
-
-import java.util.Date;
-import java.util.List;
-
-import models.BoxScore;
 import models.Game;
 import models.Game.ProcessingType;
-import util.DateTime;
-import actor.ActorApi.CompleteGame;
-import actor.ActorApi.GameId;
-import actor.ActorApi.GameIds;
-import actor.ActorApi.ModelException;
-import actor.ActorApi.ScheduleGame;
 import actor.ActorApi.ServiceProps;
+import actor.ActorApi.UpdateRoster;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedActor;
@@ -23,9 +11,9 @@ import akka.actor.UntypedActor;
 public class RosterModel extends UntypedActor {
 	private ActorRef listener;
 	private final ActorRef rosterXmlStats;
-	private ActorRef gameController;
-	private String propDate;
-	private String propTeam;
+	private ActorRef controller;
+	private String gameDate;
+	private String gameTeam;
 	private ProcessingType processingType;
 	
 	public RosterModel(ActorRef listener) {
@@ -35,40 +23,16 @@ public class RosterModel extends UntypedActor {
 
 	public void onReceive(Object message) {
 		if (message instanceof ServiceProps) {
-			propDate = ((ServiceProps) message).date == null ? DateTime.getFindDateShort(new Date()) : ((ServiceProps) message).date;
-			propTeam = ((ServiceProps) message).team;
 			processingType = Game.ProcessingType.valueOf(((ServiceProps) message).processType);
 			rosterXmlStats.tell(message, getSender());
 		}
-		else if (message.equals(WorkStart)) {					
-//			List<Long> games = null;
-//			gameController = getSender();
-//			try {
-//				if (propTeam == null) {
-//					games = Game.findIdsByDateSize(propDate, propSize, processingType);
-//				}
-//				else {
-//					games = Game.findIdsByDateTeamSize(propDate, propTeam, propSize, processingType);
-//				}
-//				if (games == null) {
-//					throw new NullPointerException();
-//				}
-//			} catch (NullPointerException e) {
-//				getContext().stop(getSelf());
-//				ModelException me = new ModelException("NoGamesFound");
-//				listener.tell(me, getSelf());
-//			}
-//			GameIds ids = new GameIds(games);
-//			getSender().tell(ids, getSelf());
+		else if(message instanceof UpdateRoster) {
+			controller = getSender();			
+			gameDate = ((UpdateRoster) message).date;
+			gameTeam = ((UpdateRoster) message).team;
+		
+			rosterXmlStats.tell(message, getSelf());
 		}
-		else if(message instanceof CompleteGame) {
-			Game game = ((CompleteGame)message).game;
-			BoxScore awayBoxScore = game.getBoxScores().get(0);
-			BoxScore homeBoxScore = game.getBoxScores().get(1);
-			System.out.println(awayBoxScore.getTeam().getShortName() +  " " + awayBoxScore.getPoints() + " " + homeBoxScore.getTeam().getShortName() +  " " + homeBoxScore.getPoints());
-		  	Game.update(game, processingType);
-		  	gameController.tell(NextGame, getSelf());
-		}		
 		else {
 			unhandled(message);
 		}
