07ad661563aeb592e9281e4ac1606d9e4f81b45a
Paul Rossotti <rossotti.paul@gmail.com>
2014-06-27 21:11:05 -0700

    Move enum tests to UtilitiesTest

--- basketball/test/utilities/UtilitiesTest.java
+++ basketball/test/utilities/UtilitiesTest.java
@@ -1,6 +1,8 @@
 package utilities;
 
 import static org.fest.assertions.Assertions.assertThat;
+import models.RosterPlayer;
+import models.RosterPlayer.Position;
 
 import org.junit.Test;
 
@@ -16,5 +18,11 @@ public class UtilitiesTest {
     @Test
     public void padString() {
    		assertThat(Utilities.padString("Jump", 10)).isEqualTo("Jump      ");
+    }
+    
+    @Test
+    public void convertStringToEnum() {
+    	Position position = RosterPlayer.Position.valueOf("C");
+    	assertThat(position.equals(Position.C));
     }
 }
